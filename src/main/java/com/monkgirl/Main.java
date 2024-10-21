51ef85b7ae8907d144d6dad8624eb6e8d5e95c7c
MonkGirl <monkgirl2014@gmail.com>
2024-10-21 17:22:15 +0800

    feat(*): init sonar cloud
    sonar cloud

--- src/main/java/com/monkgirl/Main.java
+++ src/main/java/com/monkgirl/Main.java
@@ -1,11 +1,28 @@
 package com.monkgirl;
 
+import java.util.logging.Logger;
+
 /**
-* @author MissYoung
-* @since 2024-10-21
-* @version 0.1
-*/public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+ * @author MissYoung
+ * @version 0.1
+ * @since 2024-10-21
+ */
+public final class Main {
+    private Main() {
+
+    }
+
+    /**
+     * logger.
+     */
+    private static final Logger LOGGER = Logger.getLogger("Main");
+
+    /**
+     * Main method.
+     *
+     * @param args args
+     */
+    public static void main(final String[] args) {
+        LOGGER.info("Hello World!");
     }
 }
