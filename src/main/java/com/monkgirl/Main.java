b24ed26c051f748daa651a9e9a36e24e8af4652b
MonkGirl <monkgirl2014@gmail.com>
2024-10-21 16:51:17 +0800

    feat(*): init projects

---
+++ src/main/java/com/monkgirl/Main.java
@@ -0,0 +1,11 @@
+package com.monkgirl;
+
+/**
+* @author MissYoung
+* @since 2024-10-21
+* @version 0.1
+*/public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
